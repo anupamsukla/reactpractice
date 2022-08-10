3c32a6c73cad28e13438c92fb3172a1d254b9cb5
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-08-10 05:40:24 +0530

    ok

--- frontend/todoappredux/src/App.js
+++ frontend/todoappredux/src/App.js
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
 import { add, remove, reset } from './redux/action'
+import { store } from './redux/store'
 
 function App() {
   const [name, setname] = useState('')
-  const dispatch = useDispatch()
-  const { data } = useSelector((state) => state.todo)
+  // const dispatch = useDispatch()
+  // const { data } = useSelector((state) => state.todo)
+  const [data, setData] = useState(() => {
+    return []
+  })
+
+  useEffect(() => {
+    updatestate()
+    store.subscribe(updatestate)
+  }, [])
+
+  const updatestate = () => {
+    const state = store.getState()
+    setData(state.todo.data)
+  }
 
   const deleteitem = (idx) => {
-    console.log(idx)
-    dispatch(remove(idx))
+    store.dispatch(remove(idx))
   }
   return (
     <div className="App">
@@ -21,9 +34,9 @@ function App() {
         onChange={(e) => setname(e.target.value)}
         type="text"
       />
-      <div onClick={() => dispatch(add(name))}>add</div>
+      <div onClick={() => store.dispatch(add(name))}>add</div>
       <div>{JSON.stringify(data)}</div>
-      <div onClick={() => dispatch(reset(name))}>RESET</div>
+      <div onClick={() => store.dispatch(reset(name))}>RESET</div>
       <div onClick={remove}>
         {data.map((e, idx) => (
           <h1 key={idx} onClick={() => deleteitem(idx)}>
